6dc4f07a2474078fcc2afd397c493bd0c3a1fea7
komal <komal@urbanvaluate.ai>
2026-09-24 01:42:51 +0530

    fix(routes): use vercel routes to preserve path info and  static forwarding

--- app.py
+++ app.py
@@ -426,9 +426,6 @@ def generate_surveyor_memo(features, predicted_price, loc_profile, financials):
 # ----------------- PAGE ROUTES ----------------- #
 
 @app.route('/')
-@app.route('/api/index')
-@app.route('/api/index/')
-@app.route('/api/index.py')
 def home():
     """Renders the landing home page."""
     return render_template('index.html', metrics=model_metrics)
